e97395819192933c35aa6e4a4a6d7665d860e9f5
MikeBLambert <mlamb128@gmail.com>
2018-01-24 21:26:14 -0800

    add html and JS allowing users to count to another number

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Business Logic
+function letsCount(countBy,countTo) {
+  var countBy = parseInt(countBy);
+  var countTo = parseInt(countTo);
+  var set = [];
+  for (var index = countBy;index <= countTo; index += countBy){
+    set.push(index+" ");
+  };
+
+  return set;
+  alert(set);
+}
+
+//User Interface
+$(document).ready(function() {
+  $("#countParameters").submit(function(event) {
+  event.preventDefault();
+  var countBy = $("#countBy").val();
+  var countTo = $("#countTo").val();
+  var finalSet= letsCount(countBy,countTo);
+  $("#printSet").html(finalSet);
+  });
+});
